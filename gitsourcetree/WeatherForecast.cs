f15c477f4f52763e64d0916423b564e0c0651393
DESKTOP-AUDUNEO\TCS <mhinaumer@gmail.com>
2023-03-01 11:52:42 +0500

    added summary option multiple to give more choies to end user

--- gitsourcetree/WeatherForecast.cs
+++ gitsourcetree/WeatherForecast.cs
@@ -9,5 +9,11 @@ namespace gitsourcetree
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+
+        public string Summary2 { get; set; }
+
+        public string Summary3 { get; set; }  = string.Empty; 
+
+        public string Summary4 { get; set; }
     }
 }
